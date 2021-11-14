baf8d69544b07fed3de35c088e54e4aa913fec94
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-11-14 16:09:49 +0100

    Todo application models ( schemas )

---
+++ ch02/todos/model.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schmea!"
+            }
+        }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
