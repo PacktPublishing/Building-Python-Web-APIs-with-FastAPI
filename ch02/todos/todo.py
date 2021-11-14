bbac5970a0181da63e19434c8e031d9f91f40656
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-11-14 16:10:15 +0100

    Todo path operations file

---
+++ ch02/todos/todo.py
@@ -0,0 +1,68 @@
+from fastapi import APIRouter, Path
+from model import Todo, TodoItem
+
+todo_router = APIRouter()
+
+todo_list = []
+
+
+@todo_router.post("/todo")
+async def add_todo(todo: Todo):
+    todo_list.append(todo)
+    return {
+        "message": "Todo added successfully."
+    }
+
+
+@todo_router.get("/todo")
+async def retrieve_todo():
+    return {
+        "todos": todo_list
+    }
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
+
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message": "Todo updated successfully."
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int):
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "message": "Todo deleted successfully."
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
+
+
+@todo_router.delete("/todo")
+async def delete_all_todo():
+    todo_list.clear()
+    return {
+        "message": "Todos deleted successfully."
+    }
