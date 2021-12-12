008035bbaf811056d8acf263cb9002681a548319
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-12-12 19:56:20 +0100

    Code for chapter 3: Response models and Error handling

---
+++ ch03/todos/model.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schmea!"
+            }
+        }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
