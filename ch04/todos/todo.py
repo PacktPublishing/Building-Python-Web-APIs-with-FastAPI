247e2c605f31354a8dd8fea390a5e9135ffd6515
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-01-08 12:45:35 +0100

    Update todo file

---
+++ ch04/todos/todo.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Path, HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
+
+from model import Todo, TodoItem, TodoItems
+
+todo_router = APIRouter()
+
+todo_list = []
+
+templates = Jinja2Templates(directory="templates/")
+
+
+@todo_router.post("/todo")
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
+    todo_list.append(todo)
+    return templates.TemplateResponse("todo.html",
+                                      {
+                                          "request": request,
+                                          "todos": todo_list
+                                      })
+
+
+@todo_router.get("/todo", response_model=TodoItems)
+async def retrieve_todo(request: Request):
+    return templates.TemplateResponse("todo.html", {
+        "request": request,
+        "todos": todo_list
+    })
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return templates.TemplateResponse("todo.html", {
+                "request": request,
+                "todo": todo
+            })
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
+
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(request: Request, todo_data: TodoItem,
+                      todo_id: int = Path(..., title="The ID of the todo to be updated.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message": "Todo updated successfully."
+            }
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(request: Request, todo_id: int):
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "message": "Todo deleted successfully."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
+
+
+@todo_router.delete("/todo")
+async def delete_all_todo():
+    todo_list.clear()
+    return {
+        "message": "Todos deleted successfully."
+    }
