2fa387730c812b34e91f8f9fcaf3c99f939b453e
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-04-01 16:11:08 +0100

    SQL Database

---
+++ ch06/planner/database/connection.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, Session, create_engine
+
+from database.events import Events
+
+database_file = "planner.db"
+database_connection_string = f"sqlite:///{database_file}"
+
+connect_args = {"check_same_thread": False}
+engine_url = create_engine(database_connection_string, echo=True, connect_args=connect_args)
+
+def conn():
+    SQLModel.metadata.create_all(engine_url, )
+
+
+def get_session():
+    with Session(engine_url) as session:
+        yield session
---
+++ ch06/planner/database/events.py
@@ -0,0 +1,14 @@
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import List
+
+
+class Events(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    title: str
+    image: str
+    description: str
+    tags: List[str] = Field(sa_column=Column(JSON))
+    location: str
+
+    class Config:
+        arbitrary_types_allowed = True
