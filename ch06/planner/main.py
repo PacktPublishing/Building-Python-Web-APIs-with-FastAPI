3d3c062dac4cef23a444566cd41b859fab3f318d
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-04-01 16:11:36 +0100

    Mount static files

--- ch06/planner/main.py
+++ ch06/planner/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+from database.connection import conn
 
 from routes.users import user_router
 from routes.events import event_router
@@ -12,6 +15,13 @@ app = FastAPI()
 
 app.include_router(user_router,  prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
 
 
 @app.get("/")
