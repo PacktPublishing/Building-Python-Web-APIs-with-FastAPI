d387eb3459fcb0d1406e9aee102be47bad8f936c
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-04-01 16:11:45 +0100

    Update events

--- ch06/planner/routes/events.py
+++ ch06/planner/routes/events.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Body, HTTPException, Request, status
+from pathlib import Path
+
+from fastapi import APIRouter, Depends, HTTPException, Request, UploadFile, status, Form, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlmodel import select
 
-from models.events import Event
-from typing import List
+from database.connection import get_session
+from database.events import Events
 
 event_router = APIRouter(
     tags=["Events"]
@@ -11,11 +14,11 @@ event_router = APIRouter(
 
 templates = Jinja2Templates(directory="templates/")
 
-events = []
-
 
 @event_router.get("/", response_class=HTMLResponse)
-async def retrieve_all_events(request: Request):
+async def retrieve_all_events(request: Request, session=Depends(get_session)):
+    statement = select(Events)
+    events = session.exec(statement).all()
     return templates.TemplateResponse(
         "event.html",
         {
@@ -25,17 +28,27 @@ async def retrieve_all_events(request: Request):
     )
 
 
+@event_router.get("/new", response_class=HTMLResponse)
+async def add_new_event_page(request: Request):
+    return templates.TemplateResponse(
+        "new_event.html",
+        {
+            "request": request
+        }
+    )
+
+
 @event_router.get("/{id}", response_class=HTMLResponse)
-async def retrieve_event(id: int, request: Request):
-    for event in events:
-        if event.id == id:
-            return templates.TemplateResponse(
-                "event.html",
-                {
-                    "request": request,
-                    "event": event
-                }
-            )
+async def retrieve_event(id: int, request: Request, session=Depends(get_session)):
+    event = session.get(Events, id)
+    if event:
+        return templates.TemplateResponse(
+            "event.html",
+            {
+                "request": request,
+                "event": event
+            }
+        )
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
@@ -44,11 +57,31 @@ async def retrieve_event(id: int, request: Request):
 
 
 @event_router.post("/new")
-async def create_event(body: Event = Body(...)):
-    events.append(body)
-    return {
-        "message": "Event created successfully"
-    }
+async def create_event(request: Request, title: str = Form(...), description: str = Form(...), tags: str = Form(...), location: str = Form(...), image: UploadFile = File(...), session=Depends(get_session)):
+
+    uploaded_image = f"images/{image.filename}"
+    with open(uploaded_image, 'wb') as img:
+        image_content = await image.read()
+        img.write(image_content)
+
+    tags = tags.split(",")
+    new_event = Events(
+        title=title,
+        image=image.filename,
+        description=description,
+        tags=tags,
+        location=location
+    )
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+
+    event = session.get(Events, new_event.id)
+
+    return templates.TemplateResponse("event.html", {
+        "request": request,
+        "event": event
+    })
 
 
 @event_router.delete("/{id}")
