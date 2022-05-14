e07be94fc006a983a0b51b973e6e2958fdc42bc7
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-05-14 02:35:44 +0100

    Type hinted and little modifications

---
+++ ch08/planner/auth/jwt_handler.py
@@ -0,0 +1,52 @@
+import time
+from datetime import datetime
+
+from fastapi import HTTPException, status
+from jose import jwt, JWTError
+
+from database.database import Settings
+from models.users import User
+
+settings = Settings()
+
+
+def create_access_token(user: str) -> str:
+    payload = {
+        "user": user,
+        "expires": time.time() + 3600
+    }
+
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+    return token
+
+
+async def verify_access_token(token: str) -> dict:
+    try:
+        data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+
+        expire = data.get("expires")
+
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+        if datetime.utcnow() > datetime.utcfromtimestamp(expire):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Token expired!"
+            )
+        user_exist = await User.find_one(User.email == data["user"])
+        if not user_exist:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid token"
+            )
+
+        return data
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token"
+        )
