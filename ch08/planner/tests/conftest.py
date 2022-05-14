40be253d4e1350525e020d3d1c711044153cf3f9
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-05-14 02:36:03 +0100

    Tests for the API
    Signed-off-by: Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>

---
+++ ch08/planner/tests/conftest.py
@@ -0,0 +1,33 @@
+import asyncio
+import httpx
+import pytest
+
+from main import app
+from database.database import Settings
+from models.events import Event
+from models.users import User
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+async def init_db():
+    test_settings = Settings()
+    test_settings.DATABASE_URL = "mongodb://localhost:27017/testdb"
+
+    await test_settings.initialize_database()
+
+
+@pytest.fixture(scope="session")
+async def default_client():
+    await init_db()
+    async with httpx.AsyncClient(app=app, base_url="http://app") as client:
+        yield client
+
+        # Clean up resources
+        await Event.find_all().delete()
+        await User.find_all().delete()
---
+++ ch08/planner/tests/test_fixture.py
@@ -0,0 +1,20 @@
+import pytest 
+
+
+# Fixture is defined.
+from models.events import EventUpdate
+
+
+@pytest.fixture
+def event() -> EventUpdate:
+    return EventUpdate(
+        title="FastAPI Book Launch",
+        image="https://packt.com/fastapi.png",
+        description="We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
+        tags=["python", "fastapi", "book", "launch"],
+        location="Google Meet"
+    )
+
+
+def test_event_name(event: EventUpdate) -> None:
+    assert event.title == "FastAPI Book Launch"
